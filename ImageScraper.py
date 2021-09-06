a9d9757067eef26c0d5d7dc426e171f4239e5e6e
SnobbyGoblin <benjamin.reelick@gmail.com>
2021-09-07 05:06:16 +1200

    general overhaul to image scrapper script

--- ImageScraper.py
+++ ImageScraper.py
@@ -1,17 +1,104 @@
+#===========================================================================================
+# Setup 
 import praw
+import os
 
-reddit = praw.Reddit(
-    client_id='kyuTgV9ra6nCzDUnp3Dg9A',
-    client_secret='ht0lNwisfQVN7CgZ6npe3i0Sy1hY5A',
-    user_agent='my user agent' )
+#===========================================================================================
+# Classes for Image Scraper #
 
-file = open('CatPicURLs.csv', 'a')
-key = 1
+class redditIn:
+    '''Class for a reddit user instance.'''
+    def __init__(self, client_id='kyuTgV9ra6nCzDUnp3Dg9A', client_secret='ht0lNwisfQVN7CgZ6npe3i0Sy1hY5A', user_agent='my user agent'):
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.user_agent = user_agent
+        self.reddit = praw.Reddit(
+            client_id = self.client_id,
+            client_secret = self.client_secret,
+            user_agent = self.user_agent
+            )
 
-for submission in reddit.subreddit('cats').new(limit=10):
-    url = submission.url
-    if url.endswith(('.jpg', '.png', '.gif', '.jpeg')):
-        file.write('\n{},{},None'.format(key, url))
-        key += 1
+#===========================================================================================
+# Main cat image scrapper function # 
 
-file.close()
+def catImageS(redditIn, nImages, filename='CatPicURLs.csv'):
+    '''Adds a specified amount of cat picture URLs from Reddit
+    to a named file; which would be created if it does not already exist.'''
+    
+    count = 0
+    oldSubmissions = []
+    checkData = []
+    if fileExistence(filename):
+        with open(filename, "r") as f:
+            oldSubmissions = f.readlines()[1:]
+        f.close()
+        checkData = []
+        for i in range(len(oldSubmissions)):
+            oldSubmissions[i] = oldSubmissions[i].strip("\n").split(",")
+            oldSubmissions[i][1] = float(oldSubmissions[i][1])
+            checkData.append([oldSubmissions[i][0], float(oldSubmissions[i][1])])
+
+
+    # Finds submissions in the 'hot' section of the 'cats' sub-reddit 
+    # and appends their URLs with an index key.
+    submissionData = []
+    for submission in redditIn.reddit.subreddit('cats').hot(limit=None):
+        url = submission.url
+        id = submission.id
+        timeCreated = submission.created
+        if count >= nImages:
+            break
+        elif url.endswith(('.jpg', '.png', '.gif', '.jpeg')) and not checkInFile(id, timeCreated, checkData):
+            submissionData.append([id, timeCreated, url])
+            count += 1
+
+    if fileExistence(filename):
+        submissionData.extend(oldSubmissions)
+        submissionSortedData = sorted(submissionData, key = lambda x: x[1])
+        with open(filename, "w") as updated_f:
+            updated_f.write("submissionID,timeCreatedUnix,url,rating")
+            for submission in submissionSortedData:
+                updated_f.write("\n{},{},{},None".format(submission[0], submission[1], submission[2]))
+        f.close()
+    else:
+        submissionSortedData = sorted(submissionData, key = lambda x: x[1])
+        with open(filename, "w") as f:
+            f.write("submissionID,timeCreatedUnix,url,rating")
+            for submission in submissionSortedData:
+                f.write("\n{},{},{},None".format(submission[0], submission[1], submission[2]))
+            f.close()
+
+#===========================================================================================
+# File functions #
+
+def checkKey(filename):
+    '''Finds the last used key value plus 1 in the cat picture url file'''
+    with open(filename, 'rb') as f:
+        f.seek(-2, os.SEEK_END)
+        while f.read(1) != b'\n':
+            f.seek(-2, os.SEEK_CUR)
+        last_line = f.readline().decode()
+    f.close()
+    key = last_line.split(",")[0]
+    return int(key) + 1
+
+def deleteFile(filename):
+    '''Deletes specified file'''
+    os.remove(filename)
+    print("File Removed!")
+
+def fileExistence(filename):
+    '''Checks if the given file exists.'''
+    return os.path.isfile(filename)
+    
+
+def checkInFile(id, timeCreated, checkData):
+    for submission in checkData:
+        if id == submission[0] and timeCreated == checkData[1]:
+            return True
+    return False
+
+#===========================================================================================
+
+catsInstance = redditIn()
+catImageS(catsInstance, 200, filename='CatPicURLs.csv')
